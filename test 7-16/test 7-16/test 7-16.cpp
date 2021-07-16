a0037aec341fb5ead7de52929b061bbb771afcb7
foxman233 <2545826192@qq.com>
2021-07-16 22:33:42 +0800

    7月16日代码练习

---
+++ test 7-16/test 7-16/test 7-16.cpp
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+int main()
+{
+	unsigned int a = -1;
+	printf("%u\n",a);
+
+	return 0;
+}
+
+
+//int checksysten()
+//{
+//int b = 1;
+//return *(char*)&b;
+//	
+//}
+//
+//int main()
+//{
+//int a = checksysten();
+//if(a == 1)
+//{
+//	printf("Ð¡¶Ë\n");
+//}
+//else
+//printf("´ó¶Ë\n");
+//return 0;
+//}
+
+//int main()
+//{
+//	char a = 128;
+//	printf("%u",a);
+//
+//	return 0;
+//}
